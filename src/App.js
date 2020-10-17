0647c98eeddb3902d8ae9bb536704048a8a56902
Sejal Dua <sejal.dua@tufts.edu>
2020-10-17 08:30:04 -0700

    draggable and resizable tab panel = working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import { Rnd } from "react-rnd";
+import { Tabs } from "@feuer/react-tabs";
 import "./App.css";
 
 class App extends Component {
+
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = { 
+          apiResponse: "", 
+          width: 800,
+          height: 600,
+          x: 300,
+          y: 100
+        };
     }
+
+    // handleSizeChanged = size => {
+    //   this.setState({ size });
+    // };
 
     callAPI() {
         fetch("http://localhost:6789/stocks")
@@ -21,13 +34,41 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
-            </div>
+          <Rnd
+          size={{ width: this.state.width, height: this.state.height }}
+          position={{ x: this.state.x, y: this.state.y }}
+          onDragStop={(e, d) => {
+            this.setState({ x: d.x, y: d.y });
+          }}
+          onResizeStop={(e, direction, ref, delta, position) => {
+            this.setState({
+              width: ref.style.width,
+              height: ref.style.height,
+              ...position
+            });
+          }} className="container">
+              <div className="App">
+              <Tabs
+                  tabsProps={{
+                    style: {
+                      textAlign: "left"
+                    }
+                  }}
+                  activeTab={{
+                    id: "tab1"
+                  }}
+                >
+                  <Tabs.Tab id="tab1" title="Top Stocks">
+                    <div style={{ padding: 10 }}>This is tab 1</div>
+                  </Tabs.Tab>
+                  <Tabs.Tab id="tab2" title="By Sector">
+                    <div style={{ padding: 10 }}>This is tab 2</div>
+                  </Tabs.Tab>
+                </Tabs>
+                {/* <p className="App-intro">{this.state.apiResponse}</p> */}
+              </div>
+            </Rnd>
+          //  </ResizeProvider>
         );
     }
 }
